76a077a52a5144f0ceca9eb29e669a279ad13f35
Artem Tyshchuk <artemtyschukk@gmail.com>
2025-01-05 23:23:38 +0100

    Implement header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import styles from "./Header.module.scss";
+import settingsIcon from "../../assets/images/icon-settings.svg";
+import searchIcon from "../../assets/images/icon-search.svg";
+
+interface HeaderProps {
+  headerText: string;
+}
+
+export const Header = ({headerText}: HeaderProps) => {
+  return (
+    <div className={styles.header}>
+      <div>
+        <p className={styles.headerText}>{headerText}</p>
+      </div>
+
+      <div className={styles.leftSideHeader}>
+        <label className={styles.searchField}>
+          <img className={styles.searchIcon} src={searchIcon} alt="search" />
+          <input
+            className={styles.searchInput}
+            type="text"
+            name="search"
+            id="search"
+            autoComplete="off"
+            placeholder="Search by title, content, or tags…"
+          />
+        </label>
+        <button className={styles.settingsButton}>
+          <img
+            className={styles.settingsIcon}
+            src={settingsIcon}
+            alt="settings"
+          />
+        </button>
+      </div>
+
+      <span className={styles.horyzontalDivider} />
+    </div>
+  );
+};
